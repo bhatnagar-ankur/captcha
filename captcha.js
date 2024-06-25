af6d88eb82debfa1f5bc95c504deb87f9ef3ef47
bhatnagar_ankur <bhatnagar018@gmail.com>
2024-06-25 13:27:26 +0530

    updated code for module & some refactoring

--- captcha.js
+++ captcha.js
@@ -1,6 +1,6 @@
 let expectedResult = 0;
 let captchaText = "";
-let operators = [['+', function (a, b) { return a + b; }, 'plus'], ['-', function (a, b) { return a - b; }, 'minus']];
+let operators = [['+', (a, b) => { return a + b; }, 'plus'], ['-', (a, b) => { return a - b; }, 'minus']];
 let alphabets = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 let defaultOptions = {
     difficulty: 1,
@@ -9,7 +9,6 @@ let defaultOptions = {
     reload: true,
     audio: true
 };
-
 
 function initiateCaptcha(elementId, options) {
     defaultOptions = { ...defaultOptions, ...options };
@@ -21,6 +20,7 @@ function initiateCaptcha(elementId, options) {
     }
     containerElement.innerHTML = getDOMForCaptchaElements();
     fillCaptcha();
+    addEventListeners();
 }
 
 function refresh() {
@@ -53,12 +53,16 @@ function randomText() {
     let result = first.toString() + second.toString() + third.toString() + fourth.toString() + fifth.toString() + sixth.toString();
     return {
         result,
-        audioText: foo(result)
+        audioText: prepareTextForAlphabets(result)
     };
 }
 
+function addEventListeners() {
+    document.querySelector('#reloadCaptcha').addEventListener('click', refresh);
+    document.querySelector('#playCaptcha').addEventListener('click', playAudio);
+}
 
-function foo(text) {
+function prepareTextForAlphabets(text) {
     let str = '';
     for (let index = 0; index < text.length; index++) {
         if (isNaN(Number(text[index]))) {
@@ -142,7 +146,7 @@ function getRandomOperator() {
 }
 
 function validateCaptcha() {
-    const userAnswer = document.getElementById('captchaValue').value;
+    let userAnswer = document.getElementById('captchaValue').value;
     if (defaultOptions.equation) {
         userAnswer = Number(userAnswer);
     }
@@ -177,7 +181,7 @@ function playAudio_Test_Optimize() {
             msg.text = textToSpeak;
             msg.lang = 'en-US';
 
-            msg.onend = function () {
+            msg.onend = () => {
                 currentIndex++;
                 if (currentIndex < messageParts.length) {
                     // setTimeout(() => {
@@ -195,7 +199,6 @@ function playAudio_Test_Optimize() {
     }
 }
 
-
 function prepareAndAppendStyleSheet() {
     var head = document.head || document.getElementsByTagName('head')[0],
         style = document.createElement('style');
@@ -206,21 +209,23 @@ function prepareAndAppendStyleSheet() {
 
 function getDOMForCaptchaElements() {
     const basicCaptcha = `<canvas id="captcha" width="110" height="30">Sorry, your browser does not support canvas.</canvas >`;
-    const reload = `<div class="icon-container" title="Reload Captcha" onclick="refresh()">
-                    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 32 32" viewBox="0 0 32 32" id="change"><path d="M12 3H6C4.3457 3 3 4.3457 3 6v6c0 1.6543 1.3457 3 3 3h6c1.6543 0 3-1.3457 3-3V6C15 4.3457 13.6543 3 12 3zM13 12c0 .5518-.4482 1-1 1H6c-.5518 0-1-.4482-1-1V6c0-.5518.4482-1 1-1h6c.5518 0 1 .4482 1 1V12zM26 17h-6c-1.6543 0-3 1.3457-3 3v6c0 1.6543 1.3457 3 3 3h6c1.6543 0 3-1.3457 3-3v-6C29 18.3457 27.6543 17 26 17zM27 26c0 .5518-.4482 1-1 1h-6c-.5518 0-1-.4482-1-1v-6c0-.5518.4482-1 1-1h6c.5518 0 1 .4482 1 1V26zM20.9394 12.3526c.1953.1952.5118.1952.7071-.0001l.707-.707c.1953-.1953.1953-.5119-.0001-.7072L20.4144 9H24c.5523 0 1 .4477 1 1v4.5c0 .2761.2239.5.5.5h1c.2761 0 .5-.2239.5-.5V10c0-1.6569-1.3431-3-3-3h-3.5862l1.9388-1.9395c.1952-.1953.1952-.5118-.0001-.707l-.707-.707c-.1953-.1953-.5119-.1953-.7072.0001l-3.6456 3.6465c-.3904.3904-.3903 1.0235.0002 1.4139L20.9394 12.3526zM11.0606 19.6474c-.1953-.1953-.5118-.1952-.7071.0001l-.707.7069c-.1953.1953-.1953.5119.0001.7072L11.5856 23H8c-.5523 0-1-.4478-1-1v-4.5C7 17.2238 6.7761 17 6.5 17h-1C5.2239 17 5 17.2238 5 17.5V22c0 1.6569 1.3431 3 3 3h3.5862l-1.9388 1.9394c-.1952.1953-.1952.5118.0001.7071l.707.7069c.1953.1953.5119.1953.7072 0l3.6456-3.6465c.3904-.3905.3903-1.0235-.0002-1.4139L11.0606 19.6474z"></path></svg>
-                </div>`;
-    const audio = `<div class="icon-container" title="Spell Captcha" onclick="playAudio()">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="audio"><path d="M12 3a1 1 0 0 0-1 1V20a1 1 0 0 0 2 0V4A1 1 0 0 0 12 3zM8 5A1 1 0 0 0 7 6V18a1 1 0 0 0 2 0V6A1 1 0 0 0 8 5zM4 7A1 1 0 0 0 3 8v8a1 1 0 0 0 2 0V8A1 1 0 0 0 4 7zM16 5a1 1 0 0 0-1 1V18a1 1 0 0 0 2 0V6A1 1 0 0 0 16 5zM20 7a1 1 0 0 0-1 1v8a1 1 0 0 0 2 0V8A1 1 0 0 0 20 7z"></path></svg>
-                </div>`;
+    const reload = `<div id="reloadCaptcha" class="icon-container" title="Reload Captcha">
+                        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 32 32" viewBox="0 0 32 32" id="change"><path d="M12 3H6C4.3457 3 3 4.3457 3 6v6c0 1.6543 1.3457 3 3 3h6c1.6543 0 3-1.3457 3-3V6C15 4.3457 13.6543 3 12 3zM13 12c0 .5518-.4482 1-1 1H6c-.5518 0-1-.4482-1-1V6c0-.5518.4482-1 1-1h6c.5518 0 1 .4482 1 1V12zM26 17h-6c-1.6543 0-3 1.3457-3 3v6c0 1.6543 1.3457 3 3 3h6c1.6543 0 3-1.3457 3-3v-6C29 18.3457 27.6543 17 26 17zM27 26c0 .5518-.4482 1-1 1h-6c-.5518 0-1-.4482-1-1v-6c0-.5518.4482-1 1-1h6c.5518 0 1 .4482 1 1V26zM20.9394 12.3526c.1953.1952.5118.1952.7071-.0001l.707-.707c.1953-.1953.1953-.5119-.0001-.7072L20.4144 9H24c.5523 0 1 .4477 1 1v4.5c0 .2761.2239.5.5.5h1c.2761 0 .5-.2239.5-.5V10c0-1.6569-1.3431-3-3-3h-3.5862l1.9388-1.9395c.1952-.1953.1952-.5118-.0001-.707l-.707-.707c-.1953-.1953-.5119-.1953-.7072.0001l-3.6456 3.6465c-.3904.3904-.3903 1.0235.0002 1.4139L20.9394 12.3526zM11.0606 19.6474c-.1953-.1953-.5118-.1952-.7071.0001l-.707.7069c-.1953.1953-.1953.5119.0001.7072L11.5856 23H8c-.5523 0-1-.4478-1-1v-4.5C7 17.2238 6.7761 17 6.5 17h-1C5.2239 17 5 17.2238 5 17.5V22c0 1.6569 1.3431 3 3 3h3.5862l-1.9388 1.9394c-.1952.1953-.1952.5118.0001.7071l.707.7069c.1953.1953.5119.1953.7072 0l3.6456-3.6465c.3904-.3905.3903-1.0235-.0002-1.4139L11.0606 19.6474z"></path></svg>
+                    </div>`;
+    const audio = `<div id="playCaptcha" class="icon-container" title="Spell Captcha">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="audio"><path d="M12 3a1 1 0 0 0-1 1V20a1 1 0 0 0 2 0V4A1 1 0 0 0 12 3zM8 5A1 1 0 0 0 7 6V18a1 1 0 0 0 2 0V6A1 1 0 0 0 8 5zM4 7A1 1 0 0 0 3 8v8a1 1 0 0 0 2 0V8A1 1 0 0 0 4 7zM16 5a1 1 0 0 0-1 1V18a1 1 0 0 0 2 0V6A1 1 0 0 0 16 5zM20 7a1 1 0 0 0-1 1v8a1 1 0 0 0 2 0V8A1 1 0 0 0 20 7z"></path></svg>
+                    </div>`;
     return `<div class="canvas-and-btn-container">
-                ${basicCaptcha}
-                ${defaultOptions.reload ? reload : ''}
-                ${defaultOptions.audio ? audio : ''}
-            </div>
-            <input class="captcha-answer-box" type="text" id="captchaValue" placeholder="Captcha Value" />`;
+                    ${basicCaptcha}
+                    ${defaultOptions.reload ? reload : ''}
+                    ${defaultOptions.audio ? audio : ''}
+                </div>
+                <input class="captcha-answer-box" type="text" id="captchaValue" placeholder="Captcha Value" />`;
 
 }
 
 function getStyle() {
     return ``;
 }
+
+export { initiateCaptcha, validateCaptcha };
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+import { initiateCaptcha, validateCaptcha } from './captcha.js';
 initiateCaptcha('captchaContainer');
+addEventListeners();
 
 function submitForm() {
     if (validateCaptcha()) {
@@ -9,19 +11,41 @@ function submitForm() {
     event.preventDefault();
 }
 
-function captchaTextType(src) {
-    initiateCaptcha('captchaContainer', { equation: src.value === 'Equation' });
+function captchaTextType(event) {
+    const selectedValue = event.target.value;
+    initiateCaptcha('captchaContainer', { equation: selectedValue === 'Equation' });
 }
 
-function captchaLineType(src) {
-    initiateCaptcha('captchaContainer', { grid: src.value === 'Grid' });
+function captchaLineType(event) {
+    const selectedValue = event.target.value;
+    initiateCaptcha('captchaContainer', { grid: selectedValue === 'Grid' });
 }
 
-function otherFeatures(src) {
-    if (src.value === 'Audio') {
-        initiateCaptcha('captchaContainer', { audio: src.checked });
+function captchaFeatures(event) {
+    const selectedValue = event.target.value;
+    if (selectedValue === 'Audio') {
+        initiateCaptcha('captchaContainer', { audio: event.target.checked });
     }
-    if (src.value === 'Reload') {
-        initiateCaptcha('captchaContainer', { reload: src.checked });
+    if (selectedValue === 'Reload') {
+        initiateCaptcha('captchaContainer', { reload: event.target.checked });
     }
 }
+
+function addEventListeners() {
+    const radioCaptchaType = document.querySelectorAll('input[name="captchaType"]');
+    for (const radioButton of radioCaptchaType) {
+        radioButton.addEventListener('change', captchaTextType);
+    }
+
+    const radioCaptchaLineType = document.querySelectorAll('input[name="captchaLine"]');
+    for (const radioButton of radioCaptchaLineType) {
+        radioButton.addEventListener('change', captchaLineType);
+    }
+
+    const radioCaptchaFeatures = document.querySelectorAll('input[name="captchaFeatures"]');
+    for (const radioButton of radioCaptchaFeatures) {
+        radioButton.addEventListener('change', captchaFeatures);
+    }
+}
+
+export { submitForm, captchaTextType, captchaLineType, captchaFeatures }
